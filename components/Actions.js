cecdc8d1ecf4d7eb1700638e3198f250c2dbf629
Moath <moath.alhajrii@gmail.com>
2021-07-07 02:29:12 +0300

    extract actions to component

---
+++ components/Actions.js
@@ -0,0 +1,111 @@
+import prettier from 'prettier';
+import parserMarkdown from 'prettier/parser-markdown';
+
+export default function Actions({ textarea, markdown, value, setValue }) {
+    const copyMarkdown = () => {
+        textarea.current.select();
+        textarea.current.setSelectionRange(0, 99999);
+
+        document.execCommand('copy');
+
+        alert('Copied markdown text');
+    };
+
+    const copyHtml = () => {
+        const textField = document.createElement('textarea');
+        textField.innerText = markdown;
+        document.body.appendChild(textField);
+        textField.select();
+        document.execCommand('copy');
+        textField.remove();
+
+        alert('Copied html text');
+    };
+
+    const format = () => {
+        const result = prettier.format(value, {
+            parser: 'markdown',
+            plugins: [parserMarkdown],
+            printWidth: 120,
+            singleQuote: true,
+            tabWidth: 4,
+            trailingComma: 'es5',
+        });
+
+        setValue(result);
+    };
+
+    return (
+        <div className="my-5 flex flex-wrap justify-center gap-4">
+            <button className="btn" onClick={copyMarkdown}>
+                copy markdown
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8 h-8 mr-2"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <rect x="3" y="5" width="18" height="14" rx="2" />
+                    <path d="M7 15v-6l2 2l2 -2v6" />
+                    <path d="M14 13l2 2l2 -2m-2 2v-6" />
+                </svg>
+            </button>
+            <button className="btn btn-primary" onClick={copyHtml}>
+                copy html
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8 h-8 mr-2"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z" />
+                    <path d="M15.5 8h-7l.5 4h6l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5" />
+                </svg>
+            </button>
+            <button className="btn btn-accent" onClick={format}>
+                format
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8 h-8 mr-2"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M7 4a2 2 0 0 0 -2 2v3a2 3 0 0 1 -2 3a2 3 0 0 1 2 3v3a2 2 0 0 0 2 2" />
+                    <path d="M17 4a2 2 0 0 1 2 2v3a2 3 0 0 0 2 3a2 3 0 0 0 -2 3v3a2 2 0 0 1 -2 2" />
+                </svg>
+            </button>
+            <button className="btn btn-secondary" onClick={() => setValue('')}>
+                clear
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8 h-8 mr-2"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M19 19h-11l-4 -4a1 1 0 0 1 0 -1.41l10 -10a1 1 0 0 1 1.41 0l5 5a1 1 0 0 1 0 1.41l-9 9" />
+                    <line x1="18" y1="12.3" x2="11.7" y2="6" />
+                </svg>
+            </button>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 import MarkdownIt from 'markdown-it';
-import prettier from 'prettier';
-import parserMarkdown from 'prettier/parser-markdown';
 import defaultValue from '@data/markdown';
 import Head from 'next/head';
+import Actions from '@components/Actions';
 
 const parser = new MarkdownIt({
     html: false,
@@ -20,39 +19,6 @@ export default function Home() {
         setMarkdown(parser.render(value));
     }, [value]);
 
-    const copyMarkdown = () => {
-        textarea.current.select();
-        textarea.current.setSelectionRange(0, 99999);
-
-        document.execCommand('copy');
-
-        alert('Copied the text: ' + textarea.current.value);
-    };
-
-    const copyHtml = () => {
-        const textField = document.createElement('textarea');
-        textField.innerText = markdown;
-        document.body.appendChild(textField);
-        textField.select();
-        document.execCommand('copy');
-        textField.remove();
-
-        alert('Copied the text: ' + markdown);
-    };
-
-    const format = () => {
-        const result = prettier.format(value, {
-            parser: 'markdown',
-            plugins: [parserMarkdown],
-            printWidth: 120,
-            singleQuote: true,
-            tabWidth: 4,
-            trailingComma: 'es5',
-        });
-
-        setValue(result);
-    };
-
     return (
         <div className="grid grid-cols-1 lg:grid-cols-10 gap-4 min-h-screen">
             <Head>
@@ -60,77 +26,7 @@ export default function Home() {
                 <meta content="width=device-width, initial-scale=1.0, viewport-fit=cover" name="viewport" />
             </Head>
             <div className="col-span-1 lg:col-span-10">
-                <div className="my-5 flex flex-wrap justify-center gap-4">
-                    <button className="btn" onClick={copyMarkdown}>
-                        copy markdown
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-8 h-8 mr-2"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        >
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <rect x="3" y="5" width="18" height="14" rx="2" />
-                            <path d="M7 15v-6l2 2l2 -2v6" />
-                            <path d="M14 13l2 2l2 -2m-2 2v-6" />
-                        </svg>
-                    </button>
-                    <button className="btn btn-primary" onClick={copyHtml}>
-                        copy html
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-8 h-8 mr-2"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        >
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z" />
-                            <path d="M15.5 8h-7l.5 4h6l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5" />
-                        </svg>
-                    </button>
-                    <button className="btn btn-accent" onClick={format}>
-                        format
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-8 h-8 mr-2"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        >
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M7 4a2 2 0 0 0 -2 2v3a2 3 0 0 1 -2 3a2 3 0 0 1 2 3v3a2 2 0 0 0 2 2" />
-                            <path d="M17 4a2 2 0 0 1 2 2v3a2 3 0 0 0 2 3a2 3 0 0 0 -2 3v3a2 2 0 0 1 -2 2" />
-                        </svg>
-                    </button>
-                    <button className="btn btn-secondary" onClick={() => setValue('')}>
-                        clear
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-8 h-8 mr-2"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        >
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M19 19h-11l-4 -4a1 1 0 0 1 0 -1.41l10 -10a1 1 0 0 1 1.41 0l5 5a1 1 0 0 1 0 1.41l-9 9" />
-                            <line x1="18" y1="12.3" x2="11.7" y2="6" />
-                        </svg>
-                    </button>
-                </div>
+                <Actions textarea={textarea} markdown={markdown} value={value} setValue={setValue} />
             </div>
             <div className="col-span-1 lg:col-span-5 card rounded-none">
                 <div className="px-1">
