d00de721cd67ca8ae67db6fb18d298f75ef4e6a3
Moath <moath.alhajrii@gmail.com>
2021-07-17 21:23:50 +0300

    use nextjs seo

---
+++ components/SEO.js
@@ -0,0 +1,40 @@
+import { NextSeo } from 'next-seo';
+
+export default function SEO({ title = 'RTL Markdown Editor', description = 'Markdown Editor for RTL languages' }) {
+    return (
+        <NextSeo
+            title={title}
+            description={description}
+            canonical={process.env.NEXT_PUBLIC_BASE_URL}
+            openGraph={{
+                images: [{ url: `${process.env.NEXT_PUBLIC_BASE_URL}/favicon.svg` }],
+                site_name: title,
+                locale: 'ar-SA',
+                type: 'website',
+            }}
+            twitter={{
+                handle: '@Dev_Moath',
+                site: '@Dev_Moath',
+                cardType: 'summary_large_image',
+            }}
+            additionalMetaTags={[
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1.0, viewport-fit=cover',
+                },
+            ]}
+            additionalLinkTags={[
+                {
+                    rel: 'icon',
+                    type: 'image/svg+xml',
+                    href: '/favicon.svg',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    href: '/favicon.png',
+                },
+            ]}
+        />
+    );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,47 +1,11 @@
 import 'tailwindcss/tailwind.css';
 import NavBar from '@components/NavBar';
-import Head from 'next/head';
+import SEO from '@components/SEO';
 
 export default function App({ Component, pageProps }) {
-    const title = 'RTL Markdown Editor',
-        url = 'https://rtl-markdown-editor.vercel.app',
-        description = 'Markdown Editor for RTL languages';
-
     return (
         <div>
-            <Head>
-                <title>{title}</title>
-                <meta content="width=device-width, initial-scale=1.0, viewport-fit=cover" name="viewport" />
-                <link href={url} hrefLang="ar" rel="alternate" />
-                <meta content={description} name="keywords" />
-                <meta content="moath.dev" name="author" />
-                <meta content="index, follow" name="robots" />
-                <meta charSet="utf-8" />
-                <meta content={title} property="og:site_name" />
-                <meta content={url} property="og:url" />
-                <meta content="ar-SA" property="og:locale" />
-                <meta content="website" property="og:type" />
-                <meta content="summary_large_image" name="twitter:card" />
-                <meta content={url} name="twitter:site" />
-                <meta content="@dev_moath" name="twitter:creator" />
-                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-                <link rel="icon" type="image/png" href="/favicon.png" />
-                <meta name="title" content={title} />
-                <meta name="description" content={description} />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content={url} />
-                <meta property="og:title" content={title} />
-                <meta property="og:description" content={description} />
-                <meta property="og:image" content={`${url}/favicon.svg`} />
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content={url} />
-                <meta property="twitter:title" content={title} />
-                <meta property="twitter:description" content={description} />
-                <meta property="twitter:image" content={`${url}/favicon.svg`} />
-                <meta content={title} itemProp="name" />
-                <meta name="apple-mobile-web-app-title" content={title} />
-                <meta name="application-name" content={title} />
-            </Head>
+            <SEO />
             <NavBar />
             <div className="mx-3 my-5">
                 <Component {...pageProps} />
