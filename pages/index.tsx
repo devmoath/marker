b11aed6d4590631776651baf3594c173b3e8c891
Moath <moath.alhajrii@gmail.com>
2021-08-16 17:08:42 +0300

    fix bug #3

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,9 +33,9 @@ export default function Home() {
                     setHTML={setHTML}
                 />
             </div>
-            <div className="col-span-1 lg:col-span-5 min-h-[700px] max-h-[700px]">
+            <div className="col-span-1 mx-2 lg:mx-0 lg:col-span-5 min-h-[700px] max-h-[700px]">
                 <textarea
-                    className="bg-white h-full dark:bg-gray-800 dark:text-white block w-full rounded-lg border-gray-300 dark:border-white shadow-lg focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 leading-10"
+                    className="bg-white min-h-[700px] max-h-[700px] dark:bg-gray-800 dark:text-white block w-full rounded-lg border-gray-300 dark:border-white shadow-lg focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 leading-10"
                     onChange={handleMarkdownChanges}
                     value={markdown}
                     ref={textarea}
