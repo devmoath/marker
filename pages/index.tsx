718e5ff8b44844c8d4238b27195a326583f62788
Moath <moath.alhajrii@gmail.com>
2021-08-16 17:33:57 +0300

    make textarea height response for the screen height #3

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,15 +33,15 @@ export default function Home() {
                     setHTML={setHTML}
                 />
             </div>
-            <div className="col-span-1 mx-2 lg:mx-0 lg:col-span-5 min-h-[700px] max-h-[700px]">
+            <div className="col-span-1 mx-2 lg:mx-0 lg:col-span-5 lg:min-h-[700px] lg:max-h-[700px]">
                 <textarea
-                    className="bg-white min-h-[700px] max-h-[700px] dark:bg-gray-800 dark:text-white block w-full rounded-lg border-gray-300 dark:border-white shadow-lg focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 leading-10"
+                    className="bg-white h-[calc(100vh-200px)] lg:min-h-[700px] lg:max-h-[700px] dark:bg-gray-800 dark:text-white block w-full rounded-lg border-gray-300 dark:border-white shadow-lg focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 leading-10"
                     onChange={handleMarkdownChanges}
                     value={markdown}
                     ref={textarea}
                 />
             </div>
-            <div className="col-span-1 lg:col-span-5 min-h-[700px] max-h-[700px] rounded-lg shadow-lg bg-gray-50 dark:bg-gray-800 overflow-auto">
+            <div className="col-span-1 lg:col-span-5 lg:min-h-[700px] lg:max-h-[700px] rounded-lg shadow-lg bg-gray-50 dark:bg-gray-800 overflow-auto">
                 <div
                     className="prose dark:prose-light lg:prose-lg mx-5 lg:mx-10 my-10 break-words max-w-none"
                     dangerouslySetInnerHTML={{ __html: html }}
