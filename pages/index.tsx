4c3a1fe48e55c6c6863c5f99b294e9dbd9097813
Moath <moath.alhajrii@gmail.com>
2021-08-18 18:17:24 +0300

    add postcss-100vh-fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,16 +22,16 @@ export default function Home() {
     };
 
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-10 gap-x-4 gap-y-8 mx-3 md:mx-0'>
-            <div className='col-span-1 mx-2 lg:mx-0 lg:col-span-5 lg:min-h-[700px] lg:max-h-[700px]'>
+        <div className='grid grid-cols-1 lg:grid-cols-2'>
+            <div className='col-span-1'>
                 <textarea
-                    className='bg-white h-[calc(100vh-200px)] lg:min-h-[700px] lg:max-h-[700px] dark:bg-gray-800 dark:text-white block w-full rounded-lg border-gray-300 dark:border-white shadow-lg focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 leading-10'
+                    className='bg-white min-h-screen dark:bg-gray-800 dark:text-white block w-full leading-10 border-0'
                     onChange={handleMarkdownChanges}
                     value={markdown}
                     ref={textarea}
                 />
             </div>
-            <div className='col-span-1 lg:col-span-5 lg:min-h-[700px] lg:max-h-[700px] rounded-lg shadow-lg bg-gray-50 dark:bg-gray-800 overflow-auto'>
+            <div className='hidden lg:block col-span-1 max-h-screen shadow-lg bg-gray-50 dark:bg-gray-800 overflow-auto border-r-2'>
                 <div
                     className='prose dark:prose-light lg:prose-lg mx-5 lg:mx-10 my-10 break-words max-w-none'
                     dangerouslySetInnerHTML={{ __html: html }}
