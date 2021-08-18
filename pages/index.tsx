16311b95ffc4b846275561039c1bd3e2438a32e2
Moath <moath.alhajrii@gmail.com>
2021-08-18 16:40:22 +0300

    delete actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
 import defaultMarkdown from '@data/markdown';
-import Actions from '@components/Actions';
 import marked from 'marked';
 
 export default function Home() {
@@ -23,27 +22,18 @@ export default function Home() {
     };
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-10 gap-x-4 gap-y-8 mx-3 md:mx-0">
-            <div className="col-span-1 lg:col-span-10">
-                <Actions
-                    textarea={textarea}
-                    html={html}
-                    markdown={markdown}
-                    setMarkdown={setMarkdown}
-                    setHTML={setHTML}
-                />
-            </div>
-            <div className="col-span-1 mx-2 lg:mx-0 lg:col-span-5 lg:min-h-[700px] lg:max-h-[700px]">
+        <div className='grid grid-cols-1 lg:grid-cols-10 gap-x-4 gap-y-8 mx-3 md:mx-0'>
+            <div className='col-span-1 mx-2 lg:mx-0 lg:col-span-5 lg:min-h-[700px] lg:max-h-[700px]'>
                 <textarea
-                    className="bg-white h-[calc(100vh-200px)] lg:min-h-[700px] lg:max-h-[700px] dark:bg-gray-800 dark:text-white block w-full rounded-lg border-gray-300 dark:border-white shadow-lg focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 leading-10"
+                    className='bg-white h-[calc(100vh-200px)] lg:min-h-[700px] lg:max-h-[700px] dark:bg-gray-800 dark:text-white block w-full rounded-lg border-gray-300 dark:border-white shadow-lg focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 leading-10'
                     onChange={handleMarkdownChanges}
                     value={markdown}
                     ref={textarea}
                 />
             </div>
-            <div className="col-span-1 lg:col-span-5 lg:min-h-[700px] lg:max-h-[700px] rounded-lg shadow-lg bg-gray-50 dark:bg-gray-800 overflow-auto">
+            <div className='col-span-1 lg:col-span-5 lg:min-h-[700px] lg:max-h-[700px] rounded-lg shadow-lg bg-gray-50 dark:bg-gray-800 overflow-auto'>
                 <div
-                    className="prose dark:prose-light lg:prose-lg mx-5 lg:mx-10 my-10 break-words max-w-none"
+                    className='prose dark:prose-light lg:prose-lg mx-5 lg:mx-10 my-10 break-words max-w-none'
                     dangerouslySetInnerHTML={{ __html: html }}
                 />
             </div>
