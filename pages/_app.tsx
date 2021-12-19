198bccc3b7d4238a5dbb532e1bdbe4e1ca67df74
Moath <moath.alhajrii@gmail.com>
2021-12-19 20:27:36 +0300

    add PlausibleProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,26 @@
 import 'tailwindcss/tailwind.css';
 import NavBar from '@components/NavBar';
 import SEO from '@components/SEO';
-import { AppProps } from 'next/app';
-import { PreviewContext } from '@contexts/preview';
-import { useState } from 'react';
+import {AppProps} from 'next/app';
+import {PreviewContext} from '@contexts/preview';
+import {useState} from 'react';
+import PlausibleProvider from 'next-plausible';
 
-export default function App({ Component, pageProps }: AppProps) {
+
+export default function App({Component, pageProps}: AppProps) {
     const [isPreviewMode, setPreviewMode] = useState(false);
 
     return (
-        <div className="bg-white dark:bg-gray-700">
-            <PreviewContext.Provider value={{ isPreviewMode, setPreviewMode }}>
-                <SEO />
-                <NavBar />
-                <div className='pt-[4rem]'>
-                    <Component {...pageProps} />
-                </div>
-            </PreviewContext.Provider>
-        </div>
+        <PlausibleProvider domain="marker.moath.dev">
+            <div className="bg-white dark:bg-gray-700">
+                <PreviewContext.Provider value={{isPreviewMode, setPreviewMode}}>
+                    <SEO/>
+                    <NavBar/>
+                    <div className='pt-[4rem]'>
+                        <Component {...pageProps} />
+                    </div>
+                </PreviewContext.Provider>
+            </div>
+        </PlausibleProvider>
     );
 }
