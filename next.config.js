37e64f3415f5e178d287d5882b9e760b99e411a5
Moath <moath.alhajrii@gmail.com>
2022-01-22 19:42:00 +0300

    Proxying Plausible through Vercel with Next.js

---
+++ next.config.js
@@ -0,0 +1,94 @@
+const securityHeaders = [
+    {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on',
+    },
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=31536000; includeSubDomains; preload',
+    },
+    {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block',
+    },
+    {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN',
+    },
+    {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+    },
+    {
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin',
+    },
+];
+
+// noinspection JSUnusedGlobalSymbols
+module.exports = {
+    reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+        ];
+    },
+    async redirects() {
+        return [
+            {
+                source: '/twitter',
+                destination: 'https://twitter.com/intent/follow?screen_name=dev_moath',
+                permanent: true,
+            },
+            {
+                source: '/github',
+                destination: 'https://github.com/devmoath',
+                permanent: true,
+            },
+            {
+                source: '/linkedin',
+                destination: 'https://www.linkedin.com/in/moath-alhajri',
+                permanent: true,
+            },
+            {
+                source: '/youtube',
+                destination: 'https://www.youtube.com/channel/UCm-qdl6K8w6hvxFwgO7u5qw',
+                permanent: true,
+            },
+            {
+                source: '/instagram',
+                destination: 'https://www.instagram.com/dev_moath',
+                permanent: true,
+            },
+            {
+                source: '/ksu',
+                destination: 'https://ksu.edu.sa',
+                permanent: true,
+            },
+            {
+                source: '/work',
+                destination: 'https://tamkeentech.sa/',
+                permanent: true,
+            },
+        ];
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/js/script.js',
+                destination: 'https://plausible.io/js/plausible.js',
+            },
+            {
+                source: '/api/event',
+                destination: 'https://plausible.io/api/event',
+            },
+        ];
+    },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,24 @@
 import 'tailwindcss/tailwind.css';
 import NavBar from '@components/NavBar';
 import SEO from '@components/SEO';
-import {AppProps} from 'next/app';
-import {PreviewContext} from '@contexts/preview';
-import {useState} from 'react';
-import PlausibleProvider from 'next-plausible';
+import { AppProps } from 'next/app';
+import { PreviewContext } from '@contexts/preview';
+import { useState } from 'react';
+import Script from 'next/script';
 
-
-export default function App({Component, pageProps}: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
     const [isPreviewMode, setPreviewMode] = useState(false);
 
     return (
-        <PlausibleProvider domain="marker.moath.dev">
-            <div className="bg-white dark:bg-gray-700">
-                <PreviewContext.Provider value={{isPreviewMode, setPreviewMode}}>
-                    <SEO/>
-                    <NavBar/>
-                    <div className='pt-[4rem]'>
-                        <Component {...pageProps} />
-                    </div>
-                </PreviewContext.Provider>
-            </div>
-        </PlausibleProvider>
+        <div className="bg-white dark:bg-gray-700">
+            <Script defer data-domain="marker.moath.dev" src="/js/script.js" />
+            <PreviewContext.Provider value={{ isPreviewMode, setPreviewMode }}>
+                <SEO />
+                <NavBar />
+                <div className="pt-[4rem]">
+                    <Component {...pageProps} />
+                </div>
+            </PreviewContext.Provider>
+        </div>
     );
 }
