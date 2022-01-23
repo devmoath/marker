ddb0fd09365b31105e47c53011612f97eff1f697
Moath <moath.alhajrii@gmail.com>
2022-01-23 10:01:48 +0300

    remove plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,12 @@ import SEO from '@components/SEO';
 import { AppProps } from 'next/app';
 import { PreviewContext } from '@contexts/preview';
 import { useState } from 'react';
-import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
     const [isPreviewMode, setPreviewMode] = useState(false);
 
     return (
         <div className="bg-white dark:bg-gray-700">
-            <Script defer data-domain="moath.dev" src="/js/script.js" />
             <PreviewContext.Provider value={{ isPreviewMode, setPreviewMode }}>
                 <SEO />
                 <NavBar />
